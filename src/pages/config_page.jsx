b6d4d0d5c5ce5ad3d3c45a4996ed6af857b69a65
zukhu17 <maximilianojohnson10@gmail.com>
2024-11-03 18:59:00 -0300

    configuracion

--- src/pages/config_page.jsx
+++ src/pages/config_page.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../stylesheets/configpage/ConfigPage.scss';
 
 export const ConfigPage = () => {
+  const [notificaciones, setNotificaciones] = useState(false);
+  const [idioma, setIdioma] = useState('es');
+
+  const handleToggleNotificaciones = () => {
+    setNotificaciones(!notificaciones);
+  };
+
+  const handleIdiomaChange = (event) => {
+    setIdioma(event.target.value);
+  };
+
   return (
-    <>
-      <p>ustedes nos son gangsta na na na</p>
-    </>
-  )
+    <div className="config-container">
+      <div className="config-options">
+        <div className="notification-switch">
+          <label className="label-bold">
+            Activar Notificaciones
+            <div className="switch">
+              <input 
+                type="checkbox" 
+                id="notificaciones"
+                checked={notificaciones} 
+                onChange={handleToggleNotificaciones} 
+              />
+              <span className="slider"></span>
+            </div>
+          </label>
+        </div>
+
+        <div className="language-select">
+          <label className="label-bold" htmlFor="idioma">Selecciona el idioma:</label>
+          <select id="idioma" value={idioma} onChange={handleIdiomaChange}>
+            <option value="es">Español</option>
+            <option value="en">Inglés</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ConfigPage
+export default ConfigPage;
